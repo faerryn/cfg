b06f887b7311489429f70966c079554dd2dcc952
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2021-03-26 18:02:25 -0400

    config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,13 +1,13 @@
 # source profile
 if status --is-login && test -z $fish
 	set -x fish flounder
-	exec sh -l -c fish -l
+	exec sh -l -c 'exec fish -l'
 end
 
 # C/C++
-set -x CFLAGS (string split -r ' ' -- "$CFLAGS")
-set -x CXXFLAGS (string split -r ' ' -- "$CXXFLAGS")
-set -x LDFLAGS (string split -r ' ' -- "$LDFLAGS")
+set -x CFLAGS (string split -r ' ' -- $CFLAGS)
+set -x CXXFLAGS (string split -r ' ' -- $CXXFLAGS)
+set -x LDFLAGS (string split -r ' ' -- $LDFLAGS)
 
 # Rust
-set -x RUSTFLAGS (string split -r ' ' -- "$RUSTFLAGS")
+set -x RUSTFLAGS (string split -r ' ' -- $RUSTFLAGS)
