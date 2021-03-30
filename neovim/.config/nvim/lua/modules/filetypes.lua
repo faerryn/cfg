24a29d122753ca52213fd7037ff1ddfd2a52d0bb
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2021-03-30 14:03:20 -0400

    sxhkd volume and brightness control

--- neovim/.config/nvim/lua/modules/filetypes.lua
+++ neovim/.config/nvim/lua/modules/filetypes.lua
@@ -4,6 +4,8 @@ return { setup = function()
 	vim.api.nvim_command'packadd zig.vim'
 	vim.g.zig_fmt_autosave = 0
 
+	vim.api.nvim_command'packadd vim-sxhkdrc'
+
 	vim.api.nvim_exec([[
 	augroup custom_filetypes
 	autocmd!
