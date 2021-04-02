dd5ebb5b34bb2cc2852ee3748f14aac39f890116
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2021-04-02 13:05:44 -0400

    zig.vim

--- neovim/.config/nvim/lua/modules/filetypes.lua
+++ neovim/.config/nvim/lua/modules/filetypes.lua
@@ -1,9 +1,4 @@
 return { setup = function()
-	vim.api.nvim_exec([[
-	augroup custom_filetypes
-	autocmd!
-	autocmd BufNewFile *.zig setf zig
-	autocmd BufReadPost *.zig setf zig
-	augroup END
-	]], false)
+	vim.g.zig_fmt_autosave = false
+	vim.api.nvim_command'packadd zig.vim'
 end }
