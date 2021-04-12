bb328e4263c8138f2ba03d6419722f10710a037a
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2021-04-12 12:14:04 -0400

    emacs literate config

--- emacs/.config/emacs/early-init.el
+++ emacs/.config/emacs/early-init.el
@@ -1,8 +1,3 @@
-;; set user-emacs-directory to a saner location
-(setq-default
- user-emacs-directory
- (expand-file-name "emacs/" (getenv "XDG_DATA_HOME")))
-
 ;; disable unused features
 (setq-default
  inhibit-startup-screen t
@@ -12,3 +7,16 @@
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
+
+;; tangle init.org
+(let
+    ((init.org (expand-file-name "init.org" user-emacs-directory))
+     (init.el (expand-file-name "init.el" user-emacs-directory)))
+  (unless (file-newer-than-file-p init.el init.org)
+    (require 'ob-tangle)
+    (org-babel-tangle-file init.org init.el "emacs-lisp")))
+
+;; set user-emacs-directory to a saner location
+(setq-default
+ user-emacs-directory
+ (expand-file-name "emacs/" (getenv "XDG_DATA_HOME")))
