19b883725508b3cd5c25241018e1668f2201ecfe
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2021-04-13 16:51:20 -0400

    emacs forever

--- emacs/.config/emacs/early-init.el
+++ emacs/.config/emacs/early-init.el
@@ -1,30 +1,34 @@
-;; disable unused features
-(setq-default
- inhibit-startup-screen t
- package-enable-at-startup nil
- use-dialog-box nil
+;; disable garbage collection
+(setq gc-cons-threshold most-positive-fixnum)
 
+;; disable package.el
+(setq package-enable-at-startup nil)
+
+;; clean FS
+(setq
  auto-save-default nil
  create-lockfiles nil
  make-backup-files nil)
 
+;; disbale splash-screen
+(setq inhibit-startup-screen t)
+
+;; clean GUI
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
-(tooltip-mode -1)
 
-;; disable garbage collection
-(setq-default gc-cons-threshold most-positive-fixnum)
+(setq use-dialog-box nil)
 
 ;; tangle init.org
 (let
     ((init.org (expand-file-name "init.org" user-emacs-directory))
      (init.el (expand-file-name "init.el" user-emacs-directory)))
-  (unless (file-newer-than-file-p init.el init.org)
+  (when (file-newer-than-file-p init.org init.el)
     (require 'ob-tangle)
     (org-babel-tangle-file init.org init.el "emacs-lisp")))
 
-;; set user-emacs-directory to a saner location
-(setq-default
+;; sane user-emacs-directory
+(setq
  user-emacs-directory
  (expand-file-name "emacs/" (getenv "XDG_DATA_HOME")))
