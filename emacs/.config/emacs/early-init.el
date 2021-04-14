770594e4a616a830dfed453e087b5cec28eb80b5
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2021-04-14 04:52:07 -0400

    tangling init.org seems to have a major speed advantage for some reason

--- emacs/.config/emacs/early-init.el
+++ emacs/.config/emacs/early-init.el
@@ -3,3 +3,10 @@
 
 ;; disable package.el
 (setq package-enable-at-startup nil)
+;; tangle and load init.org
+(let ((init.org (expand-file-name "init.org" user-emacs-directory))
+      (init.el (expand-file-name "init.el" user-emacs-directory)))
+  (when (file-newer-than-file-p init.org init.el)
+    (require 'ob-tangle)
+    (org-babel-tangle-file init.org init.el "emacs-lisp"))
+  (load init.el nil t t))
