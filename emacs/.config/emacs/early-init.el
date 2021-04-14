98e8597440f1268e986bf5828cb3f94a2542f4db
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2021-04-14 12:40:10 -0400

    lazy-load evil nicely

--- emacs/.config/emacs/early-init.el
+++ emacs/.config/emacs/early-init.el
@@ -3,6 +3,7 @@
 
 ;; disable package.el
 (setq package-enable-at-startup nil)
+
 ;; tangle and load init.org
 (let ((init.org (expand-file-name "init.org" user-emacs-directory))
       (init.el (expand-file-name "init.el" user-emacs-directory)))
