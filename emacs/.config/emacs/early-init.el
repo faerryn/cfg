ef522a1681b88168a7d13177650327dd816a7c65
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2021-04-14 13:26:20 -0400

    don't load init.el twice!

--- emacs/.config/emacs/early-init.el
+++ emacs/.config/emacs/early-init.el
@@ -9,5 +9,4 @@
       (init.el (expand-file-name "init.el" user-emacs-directory)))
   (when (file-newer-than-file-p init.org init.el)
     (require 'ob-tangle)
-    (org-babel-tangle-file init.org init.el "emacs-lisp"))
-  (load init.el nil t t))
+    (org-babel-tangle-file init.org init.el "emacs-lisp")))
