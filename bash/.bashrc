a554f6f93f684e9bbf308dc2aa8eec9cb5edfcf7
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2021-04-13 21:27:24 -0400

    cli stuff

--- bash/.bashrc
+++ bash/.bashrc
@@ -1,3 +1,7 @@
-PS1='\w $ '
+PS1='\W ) '
 
 HISTFILE="${XDG_DATA_HOME:-${HOME}/.local/share}"/bash_history
+
+alias diff='diff --color=auto'
+alias fd='fd -H'
+alias grep='fd -H'
--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,15 +1,7 @@
 # Defined interactively
 function fish_prompt
-    if test -n "$SSH_TTY"
-        echo -n (set_color brred)"$USER"(set_color white)'@'(set_color yellow)(prompt_hostname)' '
-    end
-
-    echo -n (set_color blue)(prompt_pwd)' '
-
-    set_color -o
-    if test "$USER" = root
-        echo -n (set_color red)'# '
-    end
-    echo -n (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
+    set_color $fish_color_cwd
+    echo -n (prompt_pwd)
     set_color normal
+    echo -n ' ) '
 end
