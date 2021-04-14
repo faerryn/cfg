77a9c7abdf9bfae2169dbdb00fa4bcd7a0f7ba70
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2021-04-14 02:13:54 -0400

    use-package everything that moves

--- emacs/.config/emacs/early-init.el
+++ emacs/.config/emacs/early-init.el
@@ -1,2 +1,5 @@
+;; disable garbage collection
+(setq gc-cons-threshold most-positive-fixnum)
+
 ;; disable package.el
 (setq package-enable-at-startup nil)
--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1,10 +1,7 @@
-;; disable garbage collection
-(setq gc-cons-threshold most-positive-fixnum)
-
 ;; tangle and load config.org
 (let ((config.org (expand-file-name "config.org" user-emacs-directory))
       (config.el (expand-file-name "config.el" user-emacs-directory)))
   (when (file-newer-than-file-p config.org config.el)
     (require 'ob-tangle)
     (org-babel-tangle-file config.org config.el "emacs-lisp"))
-  (load config.el))
+  (load config.el nil t t))
