f5c3da92d806e95445dde68d52e20f53d5a7e4eb
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2021-04-15 00:10:44 -0400

    make a comment in emacs config true

--- emacs/.config/emacs/early-init.el
+++ emacs/.config/emacs/early-init.el
@@ -4,7 +4,7 @@
 ;; disable package.el
 (setq package-enable-at-startup nil)
 
-;; tangle and load init.org
+;; tangle init.org
 (let ((init.org (expand-file-name "init.org" user-emacs-directory))
       (init.el (expand-file-name "init.el" user-emacs-directory)))
   (when (file-newer-than-file-p init.org init.el)
