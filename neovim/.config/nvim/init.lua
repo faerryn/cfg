d1d81b89dea75817acb647e8fc68dcdd67b6f393
Faerryn <alexandre.liao@gmail.com>
2021-04-24 00:50:06 -0400

    neovim: termguicolors is set to true always on Windows

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -66,7 +66,7 @@ vim.o.lazyredraw = true
 
 -- colorscheme
 vim.o.background = "dark"
-vim.o.termguicolors = (os.getenv"COLORTERM" == "truecolor")
+vim.o.termguicolors = (os.getenv("COLORTERM") == "truecolor") or (vim.fn.windowsversion():len() > 0)
 vim.api.nvim_command([[autocmd ColorScheme * lua if vim.g["terminal_color_0"] then for i = 0, 15 do vim.g["terminal_color_"..i] = nil end end]])
 
 -- ripgrep for :grep
