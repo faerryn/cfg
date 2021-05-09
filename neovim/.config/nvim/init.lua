4e75f3b050f27596de6e507ee053877f0493ce6b
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2021-05-09 18:11:37 -0400

    neovim: housekeeping

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -5,16 +5,12 @@ vim.o.modeline = false
 vim.o.hidden = true
 
 -- space indents of 2
-vim.o.shiftwidth = 2
+vim.o.shiftwidth  = 2
 vim.bo.shiftwidth = 2
-vim.o.tabstop = 2
-vim.bo.tabstop = 2
-vim.o.expandtab = false
-vim.bo.expandtab = false
-
--- no swapfiles
-vim.o.swapfile = false
-vim.bo.swapfile = false
+vim.o.tabstop     = 2
+vim.bo.tabstop    = 2
+vim.o.expandtab   = false
+vim.bo.expandtab  = false
 
 -- yes undofiles
 vim.o.undofile = true
@@ -48,17 +44,13 @@ vim.o.completeopt = "menuone,noinsert"
 -- highlight matches incrementally
 vim.o.inccommand = "nosplit"
 
--- keybind timeout, swapfile and cursorhold updatetime
-vim.o.timeoutlen = 500
-vim.o.updatetime = 5000
-
 -- X11 integration
 vim.o.clipboard = "unnamedplus"
 vim.o.mouse = "ar"
 
-vim.api.nvim_set_keymap("", "<ScrollWheelUp>", "<C-Y>", { noremap = true })
-vim.api.nvim_set_keymap("", "<S-ScrollWheelUp>", "<C-U>", { noremap = true })
-vim.api.nvim_set_keymap("", "<ScrollWheelDown>", "<C-E>", { noremap = true })
+vim.api.nvim_set_keymap("", "<ScrollWheelUp>",     "<C-Y>", { noremap = true })
+vim.api.nvim_set_keymap("", "<S-ScrollWheelUp>",   "<C-U>", { noremap = true })
+vim.api.nvim_set_keymap("", "<ScrollWheelDown>",   "<C-E>", { noremap = true })
 vim.api.nvim_set_keymap("", "<S-ScrollWheelDown>", "<C-D>", { noremap = true })
 
 -- performance
@@ -82,28 +74,31 @@ vim.g.mapleader = " "
 vim.api.nvim_set_keymap("n", "<Leader>", "", { noremap = true })
 
 -- unimpaired bindings
-vim.api.nvim_set_keymap("n", "]a", "<Cmd>next<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "[a", "<Cmd>previous<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "]A", "<Cmd>last<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "[A", "<Cmd>first<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "]a",        "<Cmd>next<CR>",      { noremap = true })
+vim.api.nvim_set_keymap("n", "[a",        "<Cmd>previous<CR>",  { noremap = true })
+vim.api.nvim_set_keymap("n", "]A",        "<Cmd>last<CR>",      { noremap = true })
+vim.api.nvim_set_keymap("n", "[A",        "<Cmd>first<CR>",     { noremap = true })
 
-vim.api.nvim_set_keymap("n", "]b", "<Cmd>bnext<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "[b", "<Cmd>bprevious<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "]B", "<Cmd>blast<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "[B", "<Cmd>bfirst<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "]b",        "<Cmd>bnext<CR>",     { noremap = true })
+vim.api.nvim_set_keymap("n", "[b",        "<Cmd>bprevious<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "]B",        "<Cmd>blast<CR>",     { noremap = true })
+vim.api.nvim_set_keymap("n", "[B",        "<Cmd>bfirst<CR>",    { noremap = true })
 
-vim.api.nvim_set_keymap("n", "<Leader>l", "<Cmd>lopen<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>q", "<Cmd>copen<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<Leader>l", "<Cmd>lopen<CR>",     { noremap = true })
+vim.api.nvim_set_keymap("n", "<Leader>q", "<Cmd>copen<CR>",     { noremap = true })
 
-vim.api.nvim_set_keymap("n", "]q", "<Cmd>cnext<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "[q", "<Cmd>cprevious<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "]Q", "<Cmd>clast<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "[Q", "<Cmd>cfirst<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "]q",        "<Cmd>cnext<CR>",     { noremap = true })
+vim.api.nvim_set_keymap("n", "[q",        "<Cmd>cprevious<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "]Q",        "<Cmd>clast<CR>",     { noremap = true })
+vim.api.nvim_set_keymap("n", "[Q",        "<Cmd>cfirst<CR>",    { noremap = true })
 
-vim.api.nvim_set_keymap("n", "]l", "<Cmd>lnext<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "[l", "<Cmd>lprevious<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "]L", "<Cmd>llast<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "[L", "<Cmd>lfirst<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "]l",        "<Cmd>lnext<CR>",     { noremap = true })
+vim.api.nvim_set_keymap("n", "[l",        "<Cmd>lprevious<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "]L",        "<Cmd>llast<CR>",     { noremap = true })
+vim.api.nvim_set_keymap("n", "[L",        "<Cmd>lfirst<CR>",    { noremap = true })
+
+-- highlight yank
+vim.api.nvim_command("autocmd TextYankPost * lua vim.highlight.on_yank()")
 
 -- netrw settings
 vim.g.netrw_banner = 0
@@ -118,12 +113,12 @@ if vim.fn.executable("xdg-open") > 0 then
 end
 
 -- shell split commands
-vim.api.nvim_command("command! Shell edit term://"..vim.o.shell)
-vim.api.nvim_command("command! Hshell vsplit term://"..vim.o.shell)
+vim.api.nvim_command("command! Shell  edit           term://"..vim.o.shell)
+vim.api.nvim_command("command! Hshell vsplit         term://"..vim.o.shell)
 vim.api.nvim_command("command! Lshell topleft vsplit term://"..vim.o.shell)
-vim.api.nvim_command("command! Sshell split term://"..vim.o.shell)
-vim.api.nvim_command("command! Vshell vsplit term://"..vim.o.shell)
-vim.api.nvim_command("command! Tshell tabnew term://"..vim.o.shell)
+vim.api.nvim_command("command! Sshell split          term://"..vim.o.shell)
+vim.api.nvim_command("command! Vshell vsplit         term://"..vim.o.shell)
+vim.api.nvim_command("command! Tshell tabnew         term://"..vim.o.shell)
 
 -- bootstrap user.nvim
 local user_install_path = vim.fn.stdpath("data").."/site/pack/user/opt/faerryn/user.nvim/default/default"
@@ -139,10 +134,7 @@ local use = user.use
 use "faerryn/user.nvim"
 
 -- Fixes neovim#12587
-use {
-	"antoinemadec/FixCursorHold.nvim",
-	init = function() vim.g.cursorhold_updatetime = 500 end,
-}
+use "antoinemadec/FixCursorHold.nvim"
 
 -- command mode shortcuts
 use "ryvnf/readline.vim"
@@ -173,6 +165,9 @@ use {
 
 -- comment code in and out
 use "tomtom/tcomment_vim"
+
+-- align stuff
+use "tommcdo/vim-lion"
 
 -- correct syntax highlighting
 use {
