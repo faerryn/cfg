c7717f8477a9a4c1f258a814bc33f851295452f4
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2021-05-16 14:37:12 -0400

    dwm: brightnessctl use percentages

--- dwm/src/dwm/config.h
+++ dwm/src/dwm/config.h
@@ -72,7 +72,7 @@ static char *const autorun[] = {
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
-#define BRICTL(sign, mag) { .v = (const char*[]){ "brightnessctl", "set", mag sign, NULL } }
+#define BRICTL(sign, mag) { .v = (const char*[]){ "brightnessctl", "set", mag sign "%", NULL } }
 #define VOLCTL(sign, mag) { .v = (const char*[]){ "pulsemixer", "--change-volume", sign mag, NULL } }
 
 /* commands */
