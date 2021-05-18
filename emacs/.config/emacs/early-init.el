36bcc489207971b9e3dc9ac38f3397c2a283297f
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2021-05-18 13:02:12 -0400

    emacs: byte-compile config.el

--- emacs/.config/emacs/early-init.el
+++ emacs/.config/emacs/early-init.el
@@ -1,8 +1,12 @@
 ;;; early-init.el
 
-(let ((config.org (expand-file-name "config.org" user-emacs-directory))
-      (config.el (expand-file-name "config.el" user-emacs-directory)))
+(let* ((config (expand-file-name "config" (file-name-directory load-file-name)))
+       (config.org (concat config ".org"))
+       (config.el (concat config ".el"))
+       (config.elc (concat config ".elc")))
   (when (file-newer-than-file-p config.org config.el)
     (require 'ob-tangle)
     (org-babel-tangle-file config.org config.el "emacs-lisp"))
-  (load config.el nil nil t))
+  (when (file-newer-than-file-p config.el config.elc)
+    (byte-compile-file config.el))
+  (load config.elc nil nil t))
