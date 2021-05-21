0149305c216c8e2d7ab82f295fe575c8f053cace
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2021-05-21 11:20:19 -0400

    slstatus: show both date and time

--- slstatus/src/slstatus/config.h
+++ slstatus/src/slstatus/config.h
@@ -65,7 +65,7 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ datetime, "%s",           "%r" },
+	{ datetime, "%s",           "%c" },
 	{ wifi_essid, " | %s",      "wlan0" },
 	{ wifi_perc, " %s%%",       "wlan0" },
 	{ battery_perc, " | %s%%",  "BAT1" },
