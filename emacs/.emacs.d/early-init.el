3a7b53170f85e893aa41e319342fe29a4df438d9
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2021-06-12 18:45:30 -0400

    emacs: move init.org to config.org
    we can load config.el in early-init.el

--- emacs/.emacs.d/early-init.el
+++ emacs/.emacs.d/early-init.el
@@ -1,4 +1,4 @@
-;;; early-init.el -*- lexical-binding: t; -*-
+;;; early-config.el -*- lexical-binding: t; -*-
 
 ;; Disable default garbage collection
 (setq gc-cons-threshold most-positive-fixnum)
@@ -6,10 +6,11 @@
 ;; Disable default package manager
 (setq package-enable-at-startup nil)
 
-;; Load init.org
-(let* ((init (expand-file-name "init" (file-name-directory load-file-name)))
-       (init.org (concat init ".org"))
-       (init.el  (concat init ".el")))
-  (when (file-newer-than-file-p init.org init.el)
+;; Load config.org
+(let* ((config (expand-file-name "config" (file-name-directory load-file-name)))
+       (config.org (concat config ".org"))
+       (config.el  (concat config ".el")))
+  (when (file-newer-than-file-p config.org config.el)
     (require 'ob-tangle)
-    (org-babel-tangle-file init.org init.el "emacs-lisp")))
+    (org-babel-tangle-file config.org config.el "emacs-lisp"))
+  (load config.el nil nil t))
