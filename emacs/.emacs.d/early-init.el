7fa6fe8b7960dcc1183472df19f558e38ae9e20c
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2021-08-04 10:48:44 -0400

    emacs: simpler handling of config.org

--- emacs/.emacs.d/early-init.el
+++ emacs/.emacs.d/early-init.el
@@ -7,9 +7,8 @@
 (custom-set-variables '(package-enable-at-startup nil))
 
 ;; Load config.org
-(let* ((config (expand-file-name "config" (file-name-directory load-file-name)))
-       (config.org (concat config ".org"))
-       (config.el (concat config ".el")))
+(let* ((config.org (expand-file-name "config.org" user-emacs-directory))
+       (config.el (expand-file-name "config.el" user-emacs-directory)))
   (when (file-newer-than-file-p config.org config.el)
     (require 'ob-tangle)
     (org-babel-tangle-file config.org config.el "emacs-lisp"))
